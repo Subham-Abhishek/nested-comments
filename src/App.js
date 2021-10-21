68177d13f9e80181b982f46ad4fd918bf2b09a0a
Subham-Abhishek <abhi950727@gmail.com>
2021-10-21 20:47:56 +0530

    nested comments done and accordion added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from './data/comments'
 function App() {
   return (
     <div>
+      <h1>Nested Comments</h1>
       <Comment data = {data}/>
     </div>
   );
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,24 +1,39 @@
 import React from "react";
+import { v4 as uid } from "uuid";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export const Comment = ({ data }) => {
-    if (!data) return null;
-    else {
-      console.log(data);
+  console.log(data);
+  if (!data) return null;
+  else {
     return (
       <div>
-        {data.map((e) => {
-          return (
-            <>
-              <ul>
-                <li>
-                  <h4>{e.author}</h4>
-                  <p>{e.body}</p>
-                  <Comment data={e.replies} />
-                </li>
-              </ul>
-            </>
-          );
-        })}
+        <ul>
+          {data.map((e) => {
+            return (
+              <li key={uid()}>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header" 
+                    className="authorDetails"
+                  >
+                    <div className="name">{e.author}</div>
+                    <div className="body">{e.body}</div>
+                    <span className="btn">Reply</span>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Comment data={e.replies} />
+                  </AccordionDetails>
+                </Accordion>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
